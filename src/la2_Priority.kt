7864509fa893505c0e4b4065286b4553037e58c7
vivek <vivekrammurat@gmail.com>
2018-08-06 17:47:34 +0530

    //priority rule is (),^(power),*,/,+,-,=

---
+++ src/la2_Priority.kt
@@ -0,0 +1,14 @@
+
+
+fun main(args:Array<String>){
+
+    var n1:Int = 10
+    var n2:Int = 12
+    var n3:Int = 5
+    var sum:Int?
+
+    sum = n1 / n2 * n3 - 3 //priority rule is (),^(power),*,/,+,-,=
+
+    print("Sum is  :$sum")
+
+}
